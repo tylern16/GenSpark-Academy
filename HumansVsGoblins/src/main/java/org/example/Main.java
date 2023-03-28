87311a6bbb423359cfbb4fcc695818a6e10d4807
Tyler Nenninger <tynenninger@gmail.com>
2023-03-28 13:50:40 -0500

    human v goblin reqs

---
+++ HumansVsGoblins/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+
+//-----------------------------------------REQUIREMENTS
+//Everything is object - land/human/goblins
+//ovrride toString
+//create a grid
+//use utf characters for playyers, goblins and land
+//game is turn based: NESW
+//when a human and goblin collide, combat starts
+//combat uses math.random
+//extras:
+//-   human has inventory
+//-   goblin has drops
+//-   stats can be modified by equipment
+//-   map gen random treasure chest after each combat
+//-   goblins pursue player
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
