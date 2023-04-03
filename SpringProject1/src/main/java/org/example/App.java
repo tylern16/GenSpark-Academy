88ac72975636f87e9c9a631798816a27fa5848b1
Tyler Nenninger <tynenninger@gmail.com>
2023-04-03 15:49:01 -0500

    added classes and xml

--- SpringProject1/src/main/java/org/example/App.java
+++ SpringProject1/src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
+        
+        Student obj = (Student) context.getBean("Student1");
+        obj.sayHello();
     }
 }
+
+
+
---
+++ SpringProject1/src/main/java/org/example/Student.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Student {
+    public void sayHello() {
+        System.out.println("hai");
+    }
+}
+
+/*
+all private
+
+class student {
+    int id;
+    String name
+    list<Phone> ph;
+    Address add;
+}
+
+class Phone {
+    String mob;
+}
+
+class Address {
+    String citry;
+    String state;
+    String country:
+    String zip;
+}
+ */
