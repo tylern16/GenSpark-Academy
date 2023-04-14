48911d0a1b2eba9ba7e8a9a3d02c29bc6642502a
Tyler Nenninger <tynenninger@gmail.com>
2023-04-14 14:39:56 -0500

    Embedded db

--- securityDemo 3/src/main/java/com/example/securityDemo/Config/SecurityConfig.java
+++ securityDemo 3/src/main/java/com/example/securityDemo/Config/SecurityConfig.java
@@ -1,15 +1,29 @@
 package com.example.securityDemo.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
 
 @Configuration
 public class SecurityConfig {
@@ -19,8 +33,17 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    //MAY NOT NEED
     @Bean
-    public UserDetailsService userDetailsService() {
+    DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    };
+
+    @Bean
+    public UserDetailsService jdbcUserDetailsService(DataSource dataSource) {
         UserDetails user = User
                 .withUsername("user")
                 .password(passwordEncoder().encode("password"))
@@ -33,12 +56,18 @@ public class SecurityConfig {
                 .roles("ADMIN")
                 .build();
 
-
         //----------------USE database
-        return new InMemoryUserDetailsManager(user, admin);
+        //put user and admin into db with username, pw, and role
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(user);
+        users.createUser(admin);
+        return users;
+
+        //return new InMemoryUserDetailsManager(user, admin);
     }
 
-    @Bean
+
+
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
                 .authorizeHttpRequests()
