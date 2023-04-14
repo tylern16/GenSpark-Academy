49b6c09640f91c149cb3320fcdcba7cf84d63a86
Tyler Nenninger <tynenninger@gmail.com>
2023-04-14 08:49:50 -0500

    security practice v1

---
+++ securityDemo 3/src/main/java/com/example/securityDemo/Config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.example.securityDemo.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+
+        //----------------USE database
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/admin")
+                .hasRole("ADMIN")
+                .requestMatchers("/user")
+                .hasRole("USER")
+                .requestMatchers("/home")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+        return httpSecurity.build();
+    }
+}
---
+++ securityDemo 3/src/main/java/com/example/securityDemo/Controller/MyController.java
@@ -0,0 +1,24 @@
+package com.example.securityDemo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/user")
+    public String user() {
+        return "<HTML><H1>Welcome to User Page</H1></HTML>";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "<HTML><H1>Welcome to Admin Page</H1></HTML>";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "<HTML><H1>Welcome to Home Page</H1></HTML>";
+    }
+}
