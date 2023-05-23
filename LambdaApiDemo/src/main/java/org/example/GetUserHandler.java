1811190186f1a6c6c35f22eac479c1ff29afbf18
Tyler Nenninger <tynenninger@gmail.com>
2023-05-23 08:42:42 -0500

    html assignment 1

---
+++ LambdaApiDemo/src/main/java/org/example/GetUserHandler.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.JsonObject;
+
+import java.util.Map;
+
+public class GetUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+
+        Map<String, String> pathParameters = input.getPathParameters();
+        String userId = pathParameters.get("userId");
+
+        JsonObject returnValue = new JsonObject();
+        returnValue.addProperty("firstName", "Tyler");
+        returnValue.addProperty("lastName", "Nenninger");
+        returnValue.addProperty("userId", userId);
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.withStatusCode(200).withBody(returnValue.toString());
+
+        return response;
+    }
+}
