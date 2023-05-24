2b247fb2b7cfbf6ffd5cc7274ec5584e06e7989f
Tyler Nenninger <tynenninger@gmail.com>
2023-05-24 15:20:04 -0500

    1

--- first-project/src/app/app.module.ts
+++ first-project/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { MyFormComponent } from './my-form/my-form.component';
+import { MyFooterComponent } from './my-footer/my-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MyFormComponent,
+    MyFooterComponent
   ],
   imports: [
     BrowserModule
