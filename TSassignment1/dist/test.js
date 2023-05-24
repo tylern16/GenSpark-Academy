8e09d6e591952960eaa2916cc21eac1cc214deaf
Tyler Nenninger <tynenninger@gmail.com>
2023-05-24 14:42:37 -0500

    ts

---
+++ TSassignment1/dist/test.js
@@ -0,0 +1,6 @@
+"use strict";
+function addTwo(input) {
+    return input + 2;
+}
+console.log(addTwo(3));
+//# sourceMappingURL=test.js.map
---
+++ TSassignment1/src/test.ts
@@ -0,0 +1,5 @@
+function addTwo(input: number): number {
+    return input + 2;
+}
+
+console.log(addTwo(3));
